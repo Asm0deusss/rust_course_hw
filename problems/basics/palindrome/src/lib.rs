f3f117b6ebc4813859a24ff87319565320e28e85
Gordei Skorobogatov <gaskorobogatov@edu.hse.ru>
2022-09-05 21:20:11 +0300

    Automatic message by rover-submit

--- problems/basics/palindrome/src/lib.rs
+++ problems/basics/palindrome/src/lib.rs
@@ -1,6 +1,32 @@
 #![forbid(unsafe_code)]
 
 pub fn is_palindrome(number: u32) -> bool {
-    // TODO: your code goes here.
-    unimplemented!()
+    let mut digits = vec![];
+    let mut number_copy = number;
+
+    if number == 0 {
+        return true;
+    }
+
+    while number_copy > 0 {
+        digits.push(number_copy % 10);
+        number_copy /= 10;
+    }
+
+    let mut left = 0;
+    let mut right = digits.len() - 1;
+
+    let mut ans = true;
+
+    while left < right {
+        if digits[left] != digits[right] {
+            ans = false;
+            break;
+        } else {
+            left += 1;
+            right -= 1;
+        }
+    }
+
+    ans
 }
