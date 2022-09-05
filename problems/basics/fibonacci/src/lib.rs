8d0e0d296413352388392140b0c09db6a88e71d1
Gordei Skorobogatov <gaskorobogatov@edu.hse.ru>
2022-09-05 21:40:26 +0300

    Automatic message by rover-submit

--- problems/basics/fibonacci/src/lib.rs
+++ problems/basics/fibonacci/src/lib.rs
@@ -1,6 +1,16 @@
 #![forbid(unsafe_code)]
 
 pub fn get_nth_fibonacci(n: u32) -> u32 {
-    // TODO: your code goes here.
-    unimplemented!()
+    let mut left = 0;
+    let mut right = 1;
+    let mut counter = 0;
+
+    while counter < n {
+        let tmp = left;
+        left = right;
+        right += tmp;
+        counter += 1;
+    }
+
+    left
 }
