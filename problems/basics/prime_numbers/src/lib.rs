4204af3a808fd8da2cf6ab12838dece2ed8e4227
Gordei Skorobogatov <gaskorobogatov@edu.hse.ru>
2022-09-06 21:20:03 +0300

    Automatic message by rover-submit

--- problems/basics/prime_numbers/src/lib.rs
+++ problems/basics/prime_numbers/src/lib.rs
@@ -1,6 +1,30 @@
 #![forbid(unsafe_code)]
 
+fn is_prime(n: u32) -> bool {
+    let mut i = 2;
+    while i * i <= n {
+        if n % i == 0 {
+            return false;
+        }
+        i += 1;
+    }
+    true
+}
+
 pub fn get_n_prime_numbers(n: u32) -> Vec<u32> {
-    // TODO: your code goes here.
-    unimplemented!()
+    let mut ans = vec![];
+
+    let mut i = 2;
+
+    let mut count = 0;
+
+    while count < n {
+        if is_prime(i) {
+            ans.push(i);
+            count += 1;
+        }
+        i += 1;
+    }
+
+    ans
 }
