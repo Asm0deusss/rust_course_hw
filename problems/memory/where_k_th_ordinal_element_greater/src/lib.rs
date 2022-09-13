25d78ff3cbca8c7e3ddfa77cf092fe65c844e4fe
Gordei Skorobogatov <gaskorobogatov@edu.hse.ru>
2022-09-13 11:15:16 +0300

    Automatic message by rover-submit

--- problems/memory/where_k_th_ordinal_element_greater/src/lib.rs
+++ problems/memory/where_k_th_ordinal_element_greater/src/lib.rs
@@ -1,5 +1,22 @@
 #![forbid(unsafe_code)]
 
-pub fn where_k_th_ordinal_element_greater(lhs: &Vec<i32>, rhs: &Vec<i32>, k: usize) -> &Vec<i32> {
-    // your solution here
+pub fn where_k_th_ordinal_element_greater<'a>(
+    lhs: &'a Vec<i32>,
+    rhs: &'a Vec<i32>,
+    k: usize,
+) -> &'a Vec<i32> {
+    let mut left_copy = lhs.clone();
+    let mut right_copy = rhs.clone();
+
+    left_copy.sort();
+    right_copy.sort();
+
+    let left_stat = left_copy[k];
+    let right_stat = right_copy[k];
+
+    if left_stat <= right_stat {
+        rhs
+    } else {
+        lhs
+    }
 }
