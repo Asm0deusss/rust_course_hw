8f9e3190fa6cbeeaff3aee0f9ea6b6bcbf0cd13d
Gordei Skorobogatov <gaskorobogatov@edu.hse.ru>
2022-09-16 19:02:53 +0300

    Automatic message by rover-submit

--- problems/memory/flatten/src/lib.rs
+++ problems/memory/flatten/src/lib.rs
@@ -1,11 +1,39 @@
 #![forbid(unsafe_code)]
 
+use core::panic;
+use std::convert::TryInto;
+
 pub fn flatten<const N: usize>(data: Vec<Box<[&mut i32; N]>>) -> Vec<&mut i32> {
-    // TODO: your code goes here.
-    unimplemented!()
+    let mut ans_vec: Vec<&mut i32> = vec![];
+
+    for i in data {
+        for j in i.into_iter() {
+            ans_vec.push(j);
+        }
+    }
+
+    ans_vec
 }
 
 pub fn transform_to_fixed_arr<const N: usize>(data: &mut Vec<Vec<i32>>) -> Vec<Box<[&mut i32; N]>> {
-    // TODO: your code goes here.
-    unimplemented!()
+    for val in data.iter() {
+        if val.len() != N {
+            panic!("Inner vectors are of different size");
+        }
+    }
+
+    let mut ans_vec: Vec<Box<[&mut i32; N]>> = vec![];
+
+    for cur_vec in data.iter_mut() {
+        let mut tmp_vec: Vec<&mut i32> = vec![];
+
+        for cur_value in cur_vec.iter_mut() {
+            tmp_vec.push(cur_value);
+        }
+
+        let tmp_arr: [&mut i32; N] = tmp_vec.try_into().unwrap();
+        ans_vec.push(Box::new(tmp_arr));
+    }
+
+    ans_vec
 }
