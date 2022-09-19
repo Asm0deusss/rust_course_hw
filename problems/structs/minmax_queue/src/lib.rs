dc03aeef0f3c1c4b6c7dea9c7ae74b5b1e27a9bc
Mityai <dimaz1301@gmail.com>
2022-09-19 21:40:03 +0300

    add tasks (structs + enums + traits)

---
+++ problems/structs/minmax_queue/src/lib.rs
@@ -0,0 +1,44 @@
+#![forbid(unsafe_code)]
+
+#[derive(Default)]
+pub struct MinMaxQueue {
+    // your data here
+}
+
+impl MinMaxQueue {
+    pub fn new() -> Self {
+        // your implementation here
+    }
+
+    pub fn push(&mut self, value: i32) {
+        // your implementation here
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        // your implementation here
+    }
+
+    pub fn first(&self) -> Option<i32> {
+        // your implementation here
+    }
+
+    pub fn last(&self) -> Option<i32> {
+        // your implementation here
+    }
+
+    pub fn min(&self) -> Option<i32> {
+        // your implementation here
+    }
+
+    pub fn max(&self) -> Option<i32> {
+        // your implementation here
+    }
+
+    pub fn len(&self) -> usize {
+        // your implementation here
+    }
+
+    pub fn is_empty(&self) -> bool {
+        // your implementation here
+    }
+}
