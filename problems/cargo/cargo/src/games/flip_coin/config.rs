2dd8604929c4e1c47451a3ab25245bc9e5e02871
Kruglov Maxim <max.kruglov.work@gmail.com>
2022-09-28 16:10:15 +0300

    Add Cargo task

---
+++ problems/cargo/cargo/src/games/flip_coin/config.rs
@@ -0,0 +1,8 @@
+// TODO: your code goes here.
+unimplemented!()
+struct FlipCoinConfig {
+    pub players_proba: (f64, f64),
+    pub players: (u8, u8),
+    pub birthday_player: u8,
+    pub delta: f64,
+}
---
+++ problems/cargo/cargo/src/games/roll_dice/config.rs
@@ -0,0 +1,6 @@
+// TODO: your code goes here.
+unimplemented!()
+struct RollDiceConfig {
+    pub probas: [f64; 6],
+    pub players: (u8, u8),
+}
