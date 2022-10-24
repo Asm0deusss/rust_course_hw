c15a0570af7d60899fe63526e368634e7aef2bf9
Gordei Skorobogatov <gaskorobogatov@edu.hse.ru>
2022-10-24 16:17:57 +0300

    Automatic message by rover-submit

--- problems/iterators/take_n/src/lib.rs
+++ problems/iterators/take_n/src/lib.rs
@@ -1,5 +1,5 @@
 #![forbid(unsafe_code)]
 
 pub fn take_n(iterator: impl Iterator<Item = i32>, n: usize) -> Vec<i32> {
-    // TODO: your code goes here.
+    iterator.take(n).collect::<Vec<i32>>()
 }
